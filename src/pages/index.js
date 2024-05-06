91f47e0812d89c63bde49dd77707bfcc05f81d69
Eric <info.ericmail@gmail.com>
2024-05-06 07:41:18 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
                 className="!text-left !text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
               <p className="my-4 text-base font-medium md:text-sm sm:!text-xs">
-             The perprtual disconnect between earth and our digital lives persists. 
+             The perpetual disconnect between earth and our digital lives persists. 
              As we spend increasing amounts of time indoors, 
              algorithms learn from our data and perpetuate further indoor activities. 
              This represents only half of the problem;
